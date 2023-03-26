8cea842baa831979173461d4b374feae4cdca67c
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-26 04:31:08 -0600

    homePage displaying screenHeaderBtn

--- app/index.js
+++ app/index.js
@@ -3,12 +3,28 @@ import { View, Text, ScrollView, SafeAreaView } from "react-native";
 import { useState } from "react";
 import { Stack, useRouter } from "expo-router";
 import {COLORS, icons, images, SIZES, FONT} from './constants'
-
+import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../app/components'
 const Home = () => {
+
+  const router = useRouter();
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}} >
+      <Stack.Screen
+      options={{
+        headerStyle: {backgroundColor: COLORS.lightWhite},
+        headerShadowVisible: false,
+        headerLeft: () => (
+          <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+        ),
+        headerRight: () => (
+          <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+        ),
+        headerTitle: ""
+      }}
+      />
+
+      
+    </SafeAreaView>
   );
 };
 
