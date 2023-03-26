bfc62a93474b6f041ee6635ae56d62219854d3dd
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-26 04:17:24 -0600

    all components, images, and style cheets

--- app/index.js
+++ app/index.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { View, Text } from "react-native-web";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+import {COLORS, icons, images, SIZES, FONT} from './constants'
 
 const Home = () => {
   return (
     <View>
       <Text>Home</Text>
     </View>
-  )
-}
+  );
+};
 
 export default Home;
