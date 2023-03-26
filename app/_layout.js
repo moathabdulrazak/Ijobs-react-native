e69efd57036d0af7ba87ac4336d1f2116141be96
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-26 05:09:53 -0600

    taking a break. errors

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,22 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from 'expo-splash-screen'
 import React from "react";
 
 
- const Layout = () => {
+SplashScreen.preventAutoHideAsync();
+const Layout = () => {
+  // const [fontsLoaded] = useFonts({
+  //   DMBold: require(""),
+  //   DMMedium: require("assets\fonts\DMSans-Medium.ttf"),
+  //   DMRegular: require("C:\Users\moath\Desktop\Ijobs-react-native\assets\fonts\DMSans-Regular.ttf"),
+  // });
+
+  // if (!fontsLoaded) {
+  //   return null;
+  // }
+  
   return  <Stack/>;
  }
 
