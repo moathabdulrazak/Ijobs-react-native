08b458f23e502bdd1c731447a25b701fe8ca8d88
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-26 23:18:19 -0600

    foundation of popularJobs started

--- app/components/home/popular/Popularjobs.jsx
+++ app/components/home/popular/Popularjobs.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native'
+import { useRouter } from "expo-router"
+import { useState } from "react"
+
 
 import styles from './popularjobs.style'
 
+import {COLORS, SIZES} from '../../../constants'
+import PopularJobCard from '../../common/cards/popular/PopularJobCard.jsx'
+
 const Popularjobs = () => {
+  const router =  useRouter()
   return (
     <View>
       <Text>Popularjobs</Text>
