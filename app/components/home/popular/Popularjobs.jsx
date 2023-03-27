dbaad23f36dc261a6395afedf4b9f6c5b0665ef3
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-26 23:31:36 -0600

    foundation complete, moving to pulling from api

--- app/components/home/popular/Popularjobs.jsx
+++ app/components/home/popular/Popularjobs.jsx
@@ -11,9 +11,37 @@ import PopularJobCard from '../../common/cards/popular/PopularJobCard.jsx'
 
 const Popularjobs = () => {
   const router =  useRouter()
+  const isLoading = false;
+  const error = false;
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container} >
+      <View style={styles.header}>
+  <Text style={styles.headerTitle} >Popular jobs</Text>
+    <TouchableOpacity>
+      <Text style={styles.headerBtn} >Show all</Text>
+    </TouchableOpacity>
+      </View>
+    
+      <View style={styles.cardsContainer} > 
+    {isLoading ? (
+      <ActivityIndicator size="large" colors={COLORS.primary} />
+    ): error ? (
+      <Text>something went wrong</Text>
+    ) : (
+      <FlatList
+      data={[1, 2 , 3, 4, 5, 6, 7, 8, 9]}
+      renderItem={({item}) => (
+        <PopularJobCard
+        item={item}
+        />
+      )}
+      keyExtractor={item => item?.job_id}
+      contentContainerStyle={{columnGap: SIZES.medium}}
+      horizontal
+      />
+    ) }
+      </View>
+
     </View>
   )
 }
